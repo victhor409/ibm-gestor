b8f38a9e54ab360e037a3323d278fe9b7ad4486d
Matheus Farias <matheusfarias3456@hotmail.com>
2025-04-15 21:57:01 -0300

    criando o EmailRequest usando lombok para definir getter e setter

---
+++ gestor/src/main/java/com/ibm/model/EmailRequest.java
@@ -0,0 +1,12 @@
+package com.ibm.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailRequest {
+    private String para;
+    private String assunto;
+    private String corpo;
+}
